08568c7075bda255e5d02f2564e6403e4d1b125f
arif <arif.prasetya369@gmail.com>
2019-05-23 08:02:56 +0700

    recycler oke

--- app/src/main/java/com/example/smartjakapp/Police/PoliceUI.kt
+++ app/src/main/java/com/example/smartjakapp/Police/PoliceUI.kt
@@ -1,8 +1,8 @@
 package com.example.smartjakapp.Police
 
+import android.graphics.Color
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
 import com.example.smartjakapp.R
 import org.jetbrains.anko.*
 
@@ -11,15 +11,41 @@ import org.jetbrains.anko.*
  */
 class PoliceUI : AnkoComponent<ViewGroup> {
     override fun createView(ui: AnkoContext<ViewGroup>): View = with(ui) {
-        linearLayout {
+        relativeLayout {
             lparams(matchParent, wrapContent)
-            orientation = LinearLayout.VERTICAL
-            textView {
-                id = R.id.name
-            }.lparams(matchParent, wrapContent)
-            textView {
-                id = R.id.address
-            }.lparams(matchParent, wrapContent)
+            relativeLayout {
+                backgroundColor = resources.getColor(R.color.skyblue, null)
+                /**
+                 * Background Image
+                 */
+                imageView {
+                    setImageResource(R.drawable.police)
+                }.lparams(500, 500) {
+                    alignParentRight()
+                    alignParentBottom()
+                }
+                /**
+                 * Name
+                 */
+                textView {
+                    id = R.id.address
+                    backgroundColor = Color.GREEN
+                }.lparams(matchParent, wrapContent) {
+                    alignParentBottom()
+                }
+
+                /**
+                 * Address
+                 */
+                textView {
+                    id = R.id.name
+                    backgroundColor = Color.RED
+                }.lparams(matchParent, wrapContent) {
+                    above(R.id.address)
+                }
+            }.lparams(matchParent, matchParent) {
+                margin = 20
+            }
         }
     }
 }
