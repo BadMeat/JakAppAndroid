0a009d8ff95fae966229d2a17b30990f01f0b23e
arif <arif.prasetya369@gmail.com>
2019-05-27 07:39:04 +0700

    Map

---
+++ app/src/main/java/com/example/smartjakapp/MapBoxActivity.kt
@@ -0,0 +1,12 @@
+package com.example.smartjakapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MapBoxActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_map_box)
+    }
+}
